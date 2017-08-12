c9da582fa1cb473d3c6db8b0b45e15d13e9f60fe
Aron Wouters <lionar@mail.com>
2017-08-12 23:28:12 +0200

    Refactored http response factory.

--- source/response/factory.php
+++ source/response/factory.php
@@ -61,14 +61,28 @@ class factory
 
 	private function generate ( $content, int $status ) : \http\response
 	{
+		$response = $this->create ( $content );
+		$response->status ( $status );
+
+		foreach ( $this->headers as $key => $value )
+			$response [ $key ] = $value;
+
+		return $response;
+	}
+
+	private function create ( $content )
+	{
 		if ( $content instanceOf \http\response )
 			return $content;
-			
+		return $this->transform ( $content );
+	}
+
+	private function transform ( $content ) : \http\response
+	{
 		foreach ( $this->handlers as $handler )
 			if ( $handler->canHandle ( $content ) )
 				return $handler->handle ( $content,
-					new \http\response ( '', $status, $this->headers ) );
-
+					new \http\response ( '', 200, [ ] ) );
 		throw new cantHandleTypeException ( gettype ( $content ) );
 	}
 }
