edf5f345842d8b524dc441d38179bcd83ec3e998
Aron Wouters <lionar@mail.com>
2017-10-01 14:09:48 +0200

    Added wrong content type exception.

---
+++ source/exceptions/wrongContentTypeException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace http\exceptions;
+
+class wrongContentTypeException extends \runtimeException
+{
+	public function __construct ( string $provided, string $expected )
+	{
+		$this->message = "The content type: `{$provided}` can not be handled, expected: `{$expected}`.";
+	}
+}
