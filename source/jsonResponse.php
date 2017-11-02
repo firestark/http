bd8c702d94b1f10b33b5d5cf829f83fb50a2e63e
Aron Wouters <lionar@mail.com>
2017-11-02 14:06:51 +0100

    Merging default json headers with given headers.

--- source/jsonResponse.php
+++ source/jsonResponse.php
@@ -5,9 +5,15 @@ namespace http;
 
 class jsonResponse extends response
 {
-	protected $headers =
+	protected $jsonHeaders =
 	[
 		'Content-Type' => 'application/json',
 		'Access-Control-Allow-Headers' => 'Origin, Accept, Content-Type, Authorization, X-Requested-With, Content-Range, Content-Disposition',
 	];
+
+    public function __construct ( string $content = '', int $status = 200, array $headers = [ ] )
+    {
+        $this->headers = array_merge ( $headers, $this->jsonHeaders );
+        parent::__construct ( $content, $status, $headers );
+    }
 }
